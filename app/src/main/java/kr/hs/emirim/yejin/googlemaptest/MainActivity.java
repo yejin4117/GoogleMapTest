ebda5f9eca77454af543bcc357d742e5a7990725
yejin4117 <s2108@e-mirim.hs.kr>
2022-09-13 18:47:46 +0900

    이미지 삽입

--- app/src/main/java/kr/hs/emirim/yejin/googlemaptest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yejin/googlemaptest/MainActivity.java
@@ -1,14 +1,74 @@
 package kr.hs.emirim.yejin.googlemaptest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    GoogleMap gMap;
+    MapFragment mapFrag;
+    GroundOverlayOptions videoMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFrag.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.5082401,126.9906644),15));
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                videoMarker = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.presence_video_online)).position(latLng, 100f, 100f);
+                gMap.addGroundOverlay(videoMarker);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0,1,0,"위성지도");
+        menu.add(0,2,0,"일반지도");
+        menu.add(0,3,0,"하늘공원");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case 2:
+                gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case 3:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.5677554,126.8856819),15));
+                return true;
+        }
+        return false;
     }
 }
